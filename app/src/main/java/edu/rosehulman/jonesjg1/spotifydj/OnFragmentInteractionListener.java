0390da066b5616837a05185be5e03e5298bbfb69
austinin <austinin@rose-hulman.edu>
2018-04-22 23:24:40 -0400

    Added OnFragmentInteractionListener interface to make switching between fragments easier. (forgot to add interface)

---
+++ app/src/main/java/edu/rosehulman/jonesjg1/spotifydj/OnFragmentInteractionListener.java
@@ -0,0 +1,11 @@
+package edu.rosehulman.jonesjg1.spotifydj;
+
+/**
+ * Interface to allow for easier switching between fragments.
+ */
+
+public interface OnFragmentInteractionListener {
+
+    void changeFragment(int id);
+
+}
